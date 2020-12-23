13740d4bd81365787124eb452f82d7dcb3080eb0
Kevin March <marchkevin78@gmail.com>
2020-12-22 20:55:47 -0500

    thirdcommit

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var products = _repo.GetAllProducts();
+
+            return View(products);
+        }
+    }
+}
